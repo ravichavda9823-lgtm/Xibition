76c1712c22d2002d4b2ebb3f4ac877309ec0e55a
ravichavda9823-lgtm <ravichavda9823@gmail.com>
2026-04-30 11:32:08 +0530

    second  commit

--- Admin/src/pages/Login.jsx
+++ Admin/src/pages/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
 
   const login = async (user) => {
     let response = await axios.post(
-     `${import.meta.env.VITE_API_URL}/auth/signin`,
+      `${import.meta.env.VITE_API_URL}/auth/signin`,
       user,
     );
     return response.data;
