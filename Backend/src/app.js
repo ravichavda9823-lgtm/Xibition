1d9cb05070c5706fbb1ddde21056bccc8b065820
ravichavda9823-lgtm <ravichavda9823@gmail.com>
2026-04-30 09:24:08 +0530

    first commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","http://localhost:5174"],
+    origin: ["https://backend-8jl9.onrender.com","https://xibition.onrender.com","http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
